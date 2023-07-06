02e6dea28d97683ce383e4153a9be6a0b2654ab3
Zvyagintsev Sergey <zsv069@gmail.com>
2023-07-06 15:39:37 +0300

    Initial Commit

---
+++ src/main/java/JavaQA66Maven/App.java
@@ -0,0 +1,13 @@
+package JavaQA66Maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
